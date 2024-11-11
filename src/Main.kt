c9cc8635a332b92f754781b2b08ace51d56e1c3b
Eduardo Cercal <eduardo.cercal@pazze.app>
2024-11-11 12:59:58 -0300

    variable types

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,13 @@
 fun main() {
-    println("Hello word")
+    val char: Char = 'a'
+    val string: String = "alguma coisa aqui"
+    val boolean: Boolean = true
 
-    val nome: String = "Eduardo"
+    val double: Double = 10.0
+    val float: Float = 10f
+    val long: Long = 10L
+    val int: Int = 10
+    val short: Short = 10
+    val byte: Byte = 10
 
-    println(nome)
 }
