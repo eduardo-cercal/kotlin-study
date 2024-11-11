5d4253b45322d6211c53095e90f84cbe62fbe8bc
Eduardo Cercal <eduardo.cercal@pazze.app>
2024-11-11 14:01:23 -0300

    fixing exercises

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,3 @@
 fun main() {
-    val char: Char = 'a'
-    val string: String = "alguma coisa aqui"
-    val boolean: Boolean = true
-
-    val double: Double = 10.0
-    val float: Float = 10f
-    val long: Long = 10L
-    val int: Int = 10
-    val short: Short = 10
-    val byte: Byte = 10
-
-    val uLong: ULong = 10u
-    val uInt: UInt = 10u
-    val uShort: UShort = 10u
-    val uByte: UByte = 10u
 
 }
---
+++ src/exercicios1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //b.
+    var nomeCompleto: String = "Eduardo Cercal de Souza Aracema"
+    //c.
+    var vazio: String
+    //d.
+    val tamanhoPe: Byte = 39
+    //e.
+    val pib: Long = 1869000000000
+    //f.
+    val brasilPopulation: Int = 211000000
+    //g.
+    println(pib / brasilPopulation)
+
+}
